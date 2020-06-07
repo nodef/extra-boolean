5d1903791459ec542c13bfbce8af34414ecbfdeb
Subhajit Sahu <wolfram77@gmail.com>
2020-06-07 12:57:13 +0530

    :watermelon: cool fruit code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-exports.is = require('./is');
-exports.parse = require('./parse');
-exports.xor = require('./xor');
-exports.imp = require('./imp');
-exports.eqv = require('./eqv');
+export {default as eqv} from './eqv';
+export {default as imp} from './imp';
+export {default as is} from './is';
+export {default as parse} from './parse';
+export {default as xor} from './xor';

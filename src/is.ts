fbacf6e2e2648630895f6e168f1e6b8c6c550f97
Subhajit Sahu <wolfram77@gmail.com>
2021-01-31 21:12:10 +0530

    :bus: update is() example

--- src/is.ts
+++ src/is.ts
@@ -1,6 +1,11 @@
 /**
- * Checks if value is boolean.
+ * Check if value is boolean.
  * @param v a value
+ * ```javascript
+ * is(true)  === true
+ * is(false) === true
+ * is(0)     === false
+ * ```
  */
 function is(v: any): boolean {
   return typeof v==="boolean";
